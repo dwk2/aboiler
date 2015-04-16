e58b332a9628e68ceefdea757c8b1ceb599dce4d
David Keiser-Clark <dwk2@williams.edu>
2015-04-16 17:17:32 -0400

    Delete connDB.php

--- include/connDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// MySQL connection string
-	$connString = mysql_connect("localhost", "root", "loc$4242!pn") or 
-		die("Sorry! You lack proper authentication to the database.");
-	
-	// USE MySQL database
-	mysql_select_db("classicmodels") or
-		die(mysql_error());
-?>
