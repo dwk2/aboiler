f6067ead5e5f6deb1db79261d251849e7cf19c07
dwk2 <dwk2@williams.edu>
2015-04-16 17:15:16 -0400

    test commit
    test 1

---
+++ test.php
@@ -0,0 +1,48 @@
+<?php
+
+class SimpleClass 
+{
+	// property declaration
+	public $var = 'a default value';
+	
+	// method declaration
+	public function displayVar(){
+		echo $this->var;
+	}
+}
+
+$instance = new SimpleClass();
+
+$assigned   =  $instance;
+$reference  =& $instance;
+
+$instance->var = '$assigned will have this value';
+
+$instance = null; // $instance and $reference become null
+
+var_dump($instance);
+var_dump($reference);
+var_dump($assigned);
+
+
+class Test
+{
+    static public function getNew()
+    {
+        return new static;
+    }
+}
+
+class Child extends Test
+{}
+
+$obj1 = new Test();
+$obj2 = new $obj1;
+var_dump($obj1 !== $obj2);
+
+$obj3 = Test::getNew();
+var_dump($obj3 instanceof Test);
+
+$obj4 = Child::getNew();
+var_dump($obj4 instanceof Child);
+?>
